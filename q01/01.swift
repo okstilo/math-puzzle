2f4f74a0d6f8c8ac622641d1d367e626af308511
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-04-16 00:15:32 +0900

    Add extention to swift file

--- q01/01.swift
+++ q01/01.swift
@@ -1,14 +1,25 @@
 #!/usr/bin/swift
 
+extension Int {
+    func toString(radix i: Int = 10) -> String {
+        return String(self, radix: i)
+    }
+}
+
+extension String {
+    func reverse() -> String {
+        return String(self.characters.reversed())
+    }
+}
+
 var num = 11
-
 while true {
-    if ( String(num) == String(String(num).characters.reversed()) &&
-        String(num, radix: 8) == String(String(num, radix: 8).characters.reversed()) &&
-        String(num, radix: 2) == String(String(num, radix: 2).characters.reversed())){
-
-          print(num)
-          break
+    if ( num.toString() == num.toString().reverse() &&
+        num.toString(radix: 8) == num.toString(radix: 8).reverse() &&
+        num.toString(radix: 2) == num.toString(radix: 2).reverse()
+        ){
+        print(num)
+        break
     }
     num += 2
 }
