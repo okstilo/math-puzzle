13885fa667bc415fdc41ea6a3ac50dbae668fe96
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-04-18 00:17:20 +0900

    Add ruby file

---
+++ q02/02.rb
@@ -0,0 +1,22 @@
+#! /bin/sh
+exec ruby -S -x "$0" "$@"
+#! ruby
+
+op = ["+", "-", "*", "/", ""]
+for i in 1000..9999 do
+  c = i.to_s
+  unless c.include?("0")
+    for j in op do
+      for k in op do
+        for l in op do
+          val = c[3] + j + c[2] + k + c[1] + l + c[0]
+          if val.length > 4
+            if i == eval(val)
+            puts "#{val} = #{i}"
+            end
+          end
+        end
+      end
+    end
+  end
+end
