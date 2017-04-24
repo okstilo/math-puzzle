886ac44599184d5549f877e20cd2b22622f57c5c
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-04-24 23:35:50 +0900

    Add ruby

---
+++ q06/01.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+def is_loop(n)
+  check = n * 3 + 1
+
+  while check != 1
+    check = check.even? ? check / 2 : check * 3 + 1
+    return true if check == n
+  end
+  return false
+end
+
+puts 2.step(10000, 2).count { |i| is_loop(i) }
